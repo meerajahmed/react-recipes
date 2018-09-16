3899eadd5c2da7cc0d3716e06063ac26e153463b
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-16 12:44:03 +0530

    babel config

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var log = function log(message) {
+  return console.log(message);
+};
+
+log('Hello World');
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("Hello World");
+
+const log = (message) => console.log(message);
+
+log('Hello World');
