8d27e94d4bbc8e880ab3750608897933e92959fd
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-19 18:40:04 +0530

    webpack config

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,42 @@
-import React from "react";
-import favicon from "./favicon.png";
+import React, {Component} from "react";
+import favicon from "../favicon.png";
+import Header from "./header";
+import RecipeList from "./recipe-list";
+import RecipeDetail from "./recipe-detail";
 
-const App = () => (
-  <div>
-    <img src={favicon} alt="react recipes logo"/>
-    <h1>React Recipes</h1>
-  </div>
-);
+class App extends Component {
+
+  state = {
+    recipes: [],
+    recipeDetails: null
+  };
+
+  componentDidMount() {
+    fetch(`${API_URL}/v1/recipes`)
+      .then(response => response.json())
+      .then(recipes => this.setState(() => ({recipes})))
+  }
+
+  render() {
+    return (
+      <div>
+        <img src={favicon} alt="react recipes logo"/>
+        <Header />
+        <RecipeList
+          recipes={this.state.recipes}
+          onClickHandler={this.onClickHandler}
+        />
+        <RecipeDetail recipeDetails={this.state.recipeDetails}/>
+      </div>
+    );
+  }
+
+  onClickHandler = (id) => {
+    fetch(`${API_URL}/v1/recipes/${id}`)
+      .then(response => response.json())
+      .then(recipeDetails => this.setState(() => ({recipeDetails})))
+  };
+
+}
 
 export default App;
