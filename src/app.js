66005d7481c8b0b4055a1cd68e12cf06f1e6201a
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-22 21:55:53 +0530

    webpack config

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import favicon from "./favicon.png";
+
+const App = () => (
+  <div>
+    <img src={favicon} alt="react recipes logo"/>
+    <h1>React Recipes</h1>
+  </div>
+);
+
+export default App;
