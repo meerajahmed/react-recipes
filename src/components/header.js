781aa5e4af66fe5eb426a3aa54b81ee3ccec907c
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-26 17:51:36 +0530

    css config

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => (
   <div>
     <header>
-      <h1>My Recipes</h1>
+      <h1 className="blue">My Recipes</h1>
     </header>
   </div>
 );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
 
+import "./styles/index.css";
+
 const MOUNT_ELEMENT = document.getElementById("root");
 
 ReactDOM.render(<App />, MOUNT_ELEMENT);
