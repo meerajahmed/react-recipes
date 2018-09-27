38e95b53197141a994c5f98b8fc2352916646429
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-27 19:31:24 +0530

    apply eslint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/app";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import './styles/index.css';
 
-import "./styles/index.css";
-
-const MOUNT_ELEMENT = document.getElementById("root");
+const MOUNT_ELEMENT = document.getElementById('root');
 
 ReactDOM.render(<App />, MOUNT_ELEMENT);
