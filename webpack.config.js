da6bf6860d58ebe45fe70eae73bff3919891dacd
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-27 19:32:08 +0530

    apply eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,53 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 require('dotenv').config();
 
 module.exports = {
-  "mode": "development",
-  "context": path.join(__dirname, 'src'),
-  "entry": ['whatwg-fetch', "./index.js"],
-  "output": {
-    "path": path.join(__dirname, 'dist'),
-    "filename": "[name].[chunkhash:8].js"
+  mode: 'development',
+  context: path.join(__dirname, 'src'),
+  entry: ['whatwg-fetch', './index.js'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[chunkhash:8].js',
   },
-  "devtool": "source-map",
-  "module": {
-    "rules": [
+  devtool: 'source-map',
+  module: {
+    rules: [
       {
-        "test": /\.js$/,
-        "exclude": /node_modules/,
-        "use": {
-          "loader": "babel-loader"
-        }
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.(png|jpg|gif)$/,
         use: [
           {
-            loader: 'file-loader'
-          }
-        ]
+            loader: 'file-loader',
+          },
+        ],
       },
       {
-        "test": /\.css$/,
-        "use": [
-          "style-loader", // 3. load styles in HTML head tag
-          "css-loader", // 2. load styles using require
-          "postcss-loader" // 1. convert cssNex to normal css using config postcss.config.js
-        ]
-      }
-    ]
+        test: /\.css$/,
+        use: [
+          'style-loader', // 3. load styles in HTML head tag
+          'css-loader', // 2. load styles using require
+          'postcss-loader', // 1. convert cssNex to normal css using config postcss.config.js
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './index.html',
-      favicon: './favicon.png'
+      favicon: './favicon.png',
     }),
     new webpack.DefinePlugin({
-      API_URL: JSON.stringify(process.env.API_URL)
-    })
-  ]
+      API_URL: JSON.stringify(process.env.API_URL),
+    }),
+  ],
 };
