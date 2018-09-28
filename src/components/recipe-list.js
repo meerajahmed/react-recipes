d8f2be18ccabf6d46056265c734808851c140e47
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-28 19:35:14 +0530

    apply eslint

--- src/components/recipe-list.js
+++ src/components/recipe-list.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const RecipeList = (props) => (
-  <div style={props.style}>
+const RecipeList = ({ style, recipes, onClickHandler }) => (
+  <div style={style}>
     <h2 className="h2">Recipes</h2>
     <ul className="list-reset">
-      {props.recipes.map(({id, name, category}) => (
-        <li 
-          key={id} 
-          onClick={() => props.onClickHandler(id)}
+      {recipes.map(({ id, name, category }) => (
+        <li
+          key={id}
+          onClick={() => onClickHandler(id)}
           className="py2 border-bottom border-bottom-dashed pointer"
-          >
-            <span>{name}</span>
-            <span>{category}</span>
+        >
+          <span>{name}</span>
+          <span>{category}</span>
         </li>
       ))}
     </ul>
   </div>
 );
+
+RecipeList.propTypes = {
+  style: PropTypes.object,
+  recipes: PropTypes.array,
+  onClickHandler: PropTypes.func,
+};
 
 export default RecipeList;
