01a5fba4ba11eb129d2561a3fea268827b1977c4
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-29 19:43:55 +0530

    custom css

--- src/components/recipe-list.js
+++ src/components/recipe-list.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 const RecipeList = (props) => (
-  <div>
-    <h2>Recipes</h2>
-    <ul>
+  <div style={props.style}>
+    <h2 className="h2">Recipes</h2>
+    <ul className="list-reset">
       {props.recipes.map(({id, name, category}) => (
-        <li key={id} onClick={() => props.onClickHandler(id)}>
-          <span>{name}</span>
-          <span>{category}</span>
+        <li 
+          key={id} 
+          onClick={() => props.onClickHandler(id)}
+          className="py2 border-bottom border-bottom-dashed pointer"
+          >
+            <span>{name}</span>
+            <span>{category}</span>
         </li>
       ))}
     </ul>
