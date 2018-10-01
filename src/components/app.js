725e7fa0850c44ce2da991c443d8977afc93af44
Meeraj Ahmed <meerajahmed@gmail.com>
2018-10-01 14:27:42 +0530

    using react router

--- src/components/app.js
+++ src/components/app.js
@@ -1,46 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './home';
 import Header from './header';
-import RecipeList from './recipe-list';
-import RecipeDetail from './recipe-detail';
+import Favorites from './favorites';
 
-class App extends Component {
-  state = {
-    recipes: [],
-    recipeDetails: null,
-  };
-
-  componentDidMount() {
-    fetch(`${API_URL}/v1/recipes`)
-      .then(response => response.json())
-      .then(recipes => this.setState(() => ({ recipes })));
-  }
-
-  onClickHandler = (id) => {
-    fetch(`${API_URL}/v1/recipes/${id}`)
-      .then(response => response.json())
-      .then(recipeDetails => this.setState(() => ({ recipeDetails })));
-  };
-
-  render() {
-    const { recipes, recipeDetails } = this.state;
-    return (
-      <div>
-        <Header />
-        <main className="flex px4">
-          <RecipeList
-            recipes={recipes}
-            onClickHandler={this.onClickHandler}
-            style={{ flex: 3 }}
-          />
-          <RecipeDetail
-            recipeDetails={recipeDetails}
-            className="ml4"
-            style={{ flex: 5 }}
-          />
-        </main>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/favorites" component={Favorites} />
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
