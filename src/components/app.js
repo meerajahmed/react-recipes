3f038cc1212c463e30024e0421e7849985db1380
Meeraj Ahmed <meerajahmed@gmail.com>
2018-10-03 00:02:16 +0530

    not found route fallback

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './home';
 import Header from './header';
 import Favorites from './favorites';
+import NotFound from './not-found';
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Header />
-      <Route exact path="/" component={Home} />
-      <Route path="/favorites" component={Favorites} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/favorites" component={Favorites} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
