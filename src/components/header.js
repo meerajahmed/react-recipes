374781be3a77f2b4fed42cbd1efb92840d7936a3
Meeraj Ahmed <meerajahmed@gmail.com>
2018-10-04 00:46:24 +0530

    using HOC

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import HeaderLink from './header-link';
 import favicon from '../favicon.png';
 
 const Header = () => (
   <div>
-    <header className="px4">
-      <img src={favicon} alt="react recipes logo" />
-      <h1 className="h1 inline ml2">My Recipes</h1>
+    <header className="flex items-center justify-between px4">
+      <div>
+        <img src={favicon} alt="react recipes logo" />
+        <h1 className="h1 inline ml2">My Recipes</h1>
+      </div>
+      <nav>
+        <HeaderLink to="/">
+          {'Home'}
+        </HeaderLink>
+        <HeaderLink to="/favorites">
+          {'Favorites'}
+        </HeaderLink>
+      </nav>
     </header>
   </div>
 );
