070a2ffd4177af5b4ba0044166fc3a7c2c646226
Meeraj Ahmed <meerajahmed@gmail.com>
2018-10-04 00:47:00 +0530

    using HOC

---
+++ src/components/header-link.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const HeaderLink = ({ children, ...props }) => (
+  <NavLink
+    exact
+    className="p1 mx2 black rounded text-decoration-none"
+    activeClassName="bg-white"
+    {...props}
+  >
+    {children}
+  </NavLink>
+);
+
+export default HeaderLink;
